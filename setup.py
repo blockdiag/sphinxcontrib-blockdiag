dad9570c8ce2c811dbae055f3203614d82e4c7a5
tk0miya <i.tkomiya@gmail.com>
2014-07-02 23:01:56 +0900

    blockdiag: Bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = ['blockdiag>=1.4.1', 'Sphinx>=0.6', 'setuptools']
 
 setup(
     name='sphinxcontrib-blockdiag',
-    version='1.4.0',
+    version='1.4.1',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-blockdiag',
     license='BSD',
