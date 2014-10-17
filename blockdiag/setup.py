34aa25ebbc1e27c2dc6879b990945f7e541dea1d
tk0miya <i.tkomiya@gmail.com>
2014-10-17 14:45:23 +0900

    blockdiag: Update README

--- blockdiag/setup.py
+++ blockdiag/setup.py
@@ -2,25 +2,6 @@
 
 from setuptools import setup, find_packages
 
-long_desc = '''
-This package contains the blockdiag Sphinx extension.
-
-.. _Sphinx: http://sphinx.pocoo.org/
-.. _blockdiag: http://blockdiag.com/en/blockdiag/
-
-This extension enable you to insert block diagrams in your Sphinx document.
-Following code is sample::
-
-   .. blockdiag::
-
-      diagram {
-        A -> B -> C;
-             B -> D;
-      }
-
-
-This module needs blockdiag_.
-'''
 
 requires = ['blockdiag>=1.4.1', 'Sphinx>=0.6', 'setuptools']
 
@@ -33,7 +14,7 @@ setup(
     author='Takeshi Komiya',
     author_email='i.tkomiya@gmail.com',
     description='Sphinx "blockdiag" extension',
-    long_description=long_desc,
+    long_description=open("README.rst").read(),
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
