cafeabd4ceee3a2a0eefb3b74844cc1d540fc9c0
arto <arto@bendiken.net>
2014-11-11 00:23:21 +0100

    cheader: Ensured that the files in the manifest exist.

---
+++ tests/test_latex.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+from sphinx_testing import with_app
+from blockdiag.utils.compat import u
+
+import sys
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+blockdiag_fontpath = '/usr/share/fonts/truetype/ipafont/ipagp.ttf'
+with_png_app = with_app(srcdir='tests/docs/basic',
+                        buildername='latex',
+                        write_docstring=True,
+                        confoverrides={
+                            'latex_documents': [('index', 'test.tex', u(''), u('test'), 'manual')],
+                        })
+with_pdf_app = with_app(srcdir='tests/docs/basic',
+                        buildername='latex',
+                        write_docstring=True,
+                        confoverrides={
+                            'latex_documents': [('index', 'test.tex', u(''), u('test'), 'manual')],
+                            'blockdiag_latex_image_format': 'PDF',
+                            'blockdiag_fontpath': blockdiag_fontpath,
+                        })
+with_oldpdf_app = with_app(srcdir='tests/docs/basic',
+                           buildername='latex',
+                           write_docstring=True,
+                           confoverrides={
+                               'latex_documents': [('index', 'test.tex', u(''), u('test'), 'manual')],
+                               'blockdiag_tex_image_format': 'PDF',
+                               'blockdiag_fontpath': blockdiag_fontpath,
+                           })
+
+
+class TestSphinxcontribBlockdiagLatex(unittest.TestCase):
+    @with_png_app
+    def test_build_png_image(self, app, status, warning):
+        """
+        .. blockdiag::
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '\\\\includegraphics{.*?/latex/blockdiag-.*?.png}')
+
+    @unittest.skipUnless(os.path.exists(blockdiag_fontpath), "TrueType font not found")
+    @unittest.skipIf(sys.version_info[:2] == (3, 2), "reportlab does not support python 3.2")
+    @with_pdf_app
+    def test_build_pdf_image1(self, app, status, warning):
+        """
+        .. blockdiag::
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '\\\\includegraphics{.*?/latex/blockdiag-.*?.pdf}')
+
+    @unittest.skipUnless(os.path.exists(blockdiag_fontpath), "TrueType font not found")
+    @unittest.skipIf(sys.version_info[:2] == (3, 2), "reportlab does not support python 3.2")
+    @with_oldpdf_app
+    def test_build_pdf_image2(self, app, status, warning):
+        """
+        .. blockdiag::
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '\\\\includegraphics{.*?/latex/blockdiag-.*?.pdf}')
+
+    @with_png_app
+    def test_width_option(self, app, status, warning):
+        """
+        .. blockdiag::
+           :width: 3cm
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '\\\\includegraphics\\[width=3cm\\]{.*?/latex/blockdiag-.*?.png}')
+
+    @with_png_app
+    def test_height_option(self, app, status, warning):
+        """
+        .. blockdiag::
+           :height: 4cm
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '\\\\includegraphics\\[height=4cm\\]{.*?/latex/blockdiag-.*?.png}')
+
+    @with_png_app
+    def test_scale_option(self, app, status, warning):
+        """
+        .. blockdiag::
+           :scale: 50%
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '\\\\scalebox{0.500000}{\\\\includegraphics{.*?/latex/blockdiag-.*?.png}}')
+
+    @with_png_app
+    def test_align_option_left(self, app, status, warning):
+        """
+        .. blockdiag::
+           :align: left
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '{\\\\includegraphics{.*?/latex/blockdiag-.*?.png}\\\\hfill}')
+
+    @with_png_app
+    def test_align_option_center(self, app, status, warning):
+        """
+        .. blockdiag::
+           :align: center
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '{\\\\hfill\\\\includegraphics{.*?/latex/blockdiag-.*?.png}\\\\hfill}')
+
+    @with_png_app
+    def test_align_option_right(self, app, status, warning):
+        """
+        .. blockdiag::
+           :align: right
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '{\\\\hfill\\\\includegraphics{.*?/latex/blockdiag-.*?.png}}')
+
+    @with_png_app
+    def test_caption_option(self, app, status, warning):
+        """
+        .. blockdiag::
+           :caption: hello world
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '\\\\includegraphics{.*?/latex/blockdiag-.*?.png}')
+
+        figure = re.compile('\\\\begin{figure}\\[htbp\\]\n\\\\centering.*?'
+                            '\\\\caption{hello world}\\\\end{figure}', re.DOTALL)
+        self.assertRegexpMatches(source, figure)
+
+    @with_png_app
+    def test_caption_option_and_align_option(self, app, status, warning):
+        """
+        .. blockdiag::
+           :align: left
+           :caption: hello world
+
+           A -> B;
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '\\\\includegraphics{.*?/latex/blockdiag-.*?.png}')
+
+        figure = re.compile('\\\\begin{figure}\\[htbp\\]\\\\begin{flushleft}.*?'
+                            '\\\\caption{hello world}\\\\end{flushleft}\\\\end{figure}', re.DOTALL)
+        self.assertRegexpMatches(source, figure)
+
+    @with_png_app
+    def test_href(self, app, status, warning):
+        """
+        .. blockdiag::
+
+           A -> B;
+           A [href = ':ref:`target`'];
+        """
+        app.builder.build_all()
+        source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
+        self.assertRegexpMatches(source, '\\\\includegraphics{.*?/latex/blockdiag-.*?.png}')
